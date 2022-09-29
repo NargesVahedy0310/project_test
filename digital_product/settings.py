88c13ac959d2799957aefead9f8104cbf515e4fa
Narges <109176867+NargesVahedy0310@users.noreply.github.com>
2022-09-29 10:17:58 +0330

    Visit products in url add.

--- digital_product/settings.py
+++ digital_product/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,6 +26,7 @@ SECRET_KEY = 'django-insecure-$=th9m)l0oz30ixgfgs2b#k+i%_q75nzixhlv@3#zxhk^tj!h7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+IS_DEVEL = True
 
 ALLOWED_HOSTS = []
 
@@ -121,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
