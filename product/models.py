5c192133d5d395fe67fe2dc4d373abf18c743fc2
Narges <109176867+NargesVahedy0310@users.noreply.github.com>
2022-10-09 13:50:33 +0330

    add file type   .

--- product/models.py
+++ product/models.py
@@ -39,8 +39,17 @@ class Product(models.Model):
 
 
 class File(models.Model):
+    FILE_AUDIO = 1
+    FILE_VIDEO = 2
+    FILE_PDF = 3
+    FILE_TYPE = (
+        (FILE_AUDIO, _('file audio')),
+        (FILE_VIDEO, _('file video')),
+        (FILE_PDF, _('file pdf'))
+    )
     product = models.ForeignKey('Product', verbose_name=_('product'), related_name='files', on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
+    file_type = models.PositiveSmallIntegerField(_('file_type'), choices=FILE_TYPE)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
