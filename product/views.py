22e098351e2a7624fa030f3b6e8b4d36f5f32486
Narges <109176867+NargesVahedy0310@users.noreply.github.com>
2022-10-09 10:57:19 +0330

    add view product .

--- product/views.py
+++ product/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Category, Product, File
+from .serializers import CategorySerializer, ProductSerializer, FileSerializer
+
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class ProductDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProductSerializer(product, context={'request': request})
+        return Response(serializer.data)
